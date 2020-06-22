ce64ed8c2f7bf1b57012fc03b1766f9334b193a5
Sarah <pappas.sarah@gmail.com>
2020-06-22 11:41:51 -0700

    add component, css and types file

---
+++ src/NavButton.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import './NavButton.css';
+
+interface NavButtonProps {
+    button: Button;
+}
+
+export const NavButton: React.FC<NavButtonProps> = ({ button }) => {
+    return <div className={'navButton'}>
+        <div className={'label'}> 
+            {button.text}
+        </div>
+    </div>
+}
---
+++ src/types.d.ts
@@ -0,0 +1,4 @@
+type Button = {
+    text: string;
+};
+  

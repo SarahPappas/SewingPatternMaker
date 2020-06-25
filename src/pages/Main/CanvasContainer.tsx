6232c17ce5f93a238cbfde4aa5e92097e7033326
Audrey Morin <34172843+msdrey@users.noreply.github.com>
2020-06-25 14:04:19 -0700

    changed class CanvasDiv name to be CanvasContainer

---
+++ src/pages/Main/CanvasContainer.tsx
@@ -0,0 +1,20 @@
+import React from "react";
+import { canvasElement } from "./Canvas";
+import './CanvasContainer.css';
+
+interface CanvasContainerProps {
+    
+}
+
+const CanvasContainer: React.FC<CanvasContainerProps> = () => {
+  const canvasContainerRef = React.useRef(document.createElement("div"));
+
+  React.useEffect(()=>{
+    canvasContainerRef.current.appendChild(canvasElement);
+  }, [])
+
+  return <div className='canvasContainer' ref={canvasContainerRef}></div>
+}
+
+export { CanvasContainer };
+

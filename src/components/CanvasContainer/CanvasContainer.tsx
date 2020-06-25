476861b1cd4dd6cd2c07a595b69c485e81381b18
Audrey Morin <34172843+msdrey@users.noreply.github.com>
2020-06-25 14:20:10 -0700

    removed unused interface in canvascontainer

--- src/components/CanvasContainer/CanvasContainer.tsx
+++ src/components/CanvasContainer/CanvasContainer.tsx
@@ -2,11 +2,7 @@ import React from "react";
 import { canvasElement } from "../../canvas/Canvas";
 import './CanvasContainer.css';
 
-interface CanvasContainerProps {
-    
-}
-
-const CanvasContainer: React.FC<CanvasContainerProps> = () => {
+const CanvasContainer: React.FC = () => {
   const canvasContainerRef = React.useRef(document.createElement("div"));
 
   React.useEffect(()=>{

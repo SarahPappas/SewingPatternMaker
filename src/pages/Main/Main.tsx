9b51d682ec7909d3d091db7969044b3442a147af
Audrey Morin <34172843+msdrey@users.noreply.github.com>
2020-06-25 14:17:29 -0700

    put the canvas container files in components/canvascontainer

--- src/pages/Main/Main.tsx
+++ src/pages/Main/Main.tsx
@@ -3,7 +3,7 @@ import './Main.css';
 import { Route } from 'react-router-dom';
 import { GetStarted } from 'pages/GetStarted/GetStarted';
 import { AddPhoto } from 'pages/AddPhoto/AddPhoto';
-import { CanvasContainer } from './CanvasContainer';
+import { CanvasContainer } from '../../components/CanvasContainer/CanvasContainer';
 
 export const Main: React.FC= () => {
 

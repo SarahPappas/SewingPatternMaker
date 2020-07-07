64358c9b704f663d52cdad476cc14f863c7e4655
Sarah <pappas.sarah@gmail.com>
2020-07-07 10:51:15 -0600

    remove button that is not being used yet from Instructions page

--- src/pages/Trace/Instructions/Instructions.tsx
+++ src/pages/Trace/Instructions/Instructions.tsx
@@ -1,8 +1,5 @@
 import React from 'react';
 
-// Todo: add a 'Done' button that goes to the measurement page
-const button: Button = {text: "Done", to: "" };
-
 export const Instructions: React.FC = () => {
     return (
         <div>Instructions</div>

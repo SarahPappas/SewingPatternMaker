665179dd895548a3b3bad4130b339446b3383207
Sarah <pappas.sarah@gmail.com>
2020-07-07 10:48:24 -0600

    add alt text to arrow img

--- src/pages/GetStarted/GetStarted.tsx
+++ src/pages/GetStarted/GetStarted.tsx
@@ -8,11 +8,10 @@ const button: Button = {text: "GET STARTED", to: "/AddPhoto" };
 const intro: InstructModal = {text: ["Let’s Make a \npattern in 3 \nsteps"]};
 const steps: InstructModal = {text: ["1. Upload photo", "2. Trace pattern", "3. Print pdf"]};
 
-
 export const GetStarted: React.FC = () => {
     return (<>
         <div className={'backgroundImageTop'}></div>
-        <img className={'backgroundImageBottom'} src={backgroundArrow}></img>
+        <img className={'backgroundImageBottom'} src={backgroundArrow} alt='down arrow'></img>
         <div className={'getStartedContainer'}> 
             <InstructionModal instructModal={intro}/>
             <div className={'introSteps'}><InstructionModal instructModal={steps}/></div>

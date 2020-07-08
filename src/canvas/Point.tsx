cbeb95b6a8d1c7ff4e1cf6d445458768f3635a22
Sarah <pappas.sarah@gmail.com>
2020-07-08 13:31:59 -0600

    Add the implementation for the Point class

---
+++ src/canvas/Point.tsx
@@ -0,0 +1,17 @@
+export class Point implements IPoint {
+    private _x: number;
+    private _y: number;
+
+    constructor (x: number, y: number) {
+        this._x = x;
+        this._y = y;
+    }
+
+    getX = (): number => {
+        return this._x;
+    }
+
+    getY = (): number => {
+        return this._x;
+    }
+}

8cda57e9e3dcf46c7991374fe6f399524dc1defa
Sarah <pappas.sarah@gmail.com>
2020-07-08 16:11:04 -0600

    fix lint errors in PatternPath.tsx update addPoint to not add a duplicate point

--- src/canvas/PatternPath.tsx
+++ src/canvas/PatternPath.tsx
@@ -6,13 +6,15 @@ export class PatternPath implements IPatternPath {
     private _type: PatternPathType;
     private _pathCanvas2D: Path2D | null;
     private _isPathCanvas2DInvalid: boolean;
+    private _lastIndexAddedToCanvas2DPath: number;
 
     constructor (pathType: PatternPathType) {
         this._type = pathType;
-        this._points = new Array<Point> ();
+        this._points = new Array<Point>();
         this._pathCanvas2D = null;
         this._isPathCanvas2DInvalid = true;
-    };
+        this._lastIndexAddedToCanvas2DPath = -1;
+    }
 
     getPoints = (): Point[] => {
         return this._points;
@@ -31,14 +33,29 @@ export class PatternPath implements IPatternPath {
             return null;
         }
 
-        let currPointIndex = this._points.length - 1;
-        let currPoint = this._points[currPointIndex];
-        this._pathCanvas2D.lineTo(currPoint.getX(), currPoint.getY());
+        if (this._points.length == 1) {
+            this._pathCanvas2D.moveTo(this._points[0].getX(), this._points[0].getY());
+        }
+
+        const lastPointIndex = this._points.length - 1;
+        for (let i = this._lastIndexAddedToCanvas2DPath + 1; i < this._points.length; i++) {
+            const currPoint = this._points[i];
+            this._pathCanvas2D.lineTo(currPoint.getX(), currPoint.getY());
+        }
+    
         return this._pathCanvas2D;
     };
 
-    addPoint = (point: Point): Point[] => {
+    addPoint = (point: Point): boolean => {
+        if (this._points.length) {
+            const prevPoint = this._points[this._points.length - 1];
+            if (prevPoint.equals(point)) {
+                return false;
+            }
+        }
+        
+        this._points.push(point);
         this._isPathCanvas2DInvalid = true;
-        return this._points;
+        return true;
     };
 }

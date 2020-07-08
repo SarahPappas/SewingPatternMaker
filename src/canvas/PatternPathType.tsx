6d26de5474e1158bb5b303227d7f7cd2512328bb
Sarah <pappas.sarah@gmail.com>
2020-07-08 13:39:26 -0600

    add class patternPathType that implements IPatternPathType

---
+++ src/canvas/PatternPathType.tsx
@@ -0,0 +1,17 @@
+export class PatternPathType implements IPatternPathType {
+    private _name: PatternPathName;
+    private _color: PatternPathColor;
+
+    constructor (name: PatternPathName, color: PatternPathColor) {
+        this._name = name;
+        this._color = color;
+    }
+
+    getColor = (): PatternPathColor => {
+        return this._color;
+    }
+
+    getName = (): PatternPathName => {
+        return this._name;
+    }
+}

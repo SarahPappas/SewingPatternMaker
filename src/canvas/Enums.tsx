e91b9e9696afc39b18236cbec042106f5c41072f
Sarah <pappas.sarah@gmail.com>
2020-07-08 16:16:45 -0600

    move enums into a .tsx file

--- src/canvas/Enums.tsx
+++ src/canvas/Enums.tsx
@@ -1,10 +1,10 @@
-enum PatternPathName {
+export enum PatternPathName {
     Seam,
     Fold,
     Edge,
 }
 
-enum PatternPathColor {
+export enum PatternPathColor {
     Blue = "#248EA6",
     Green = "#84BFA4",
     Yellow = "#F2E74B",

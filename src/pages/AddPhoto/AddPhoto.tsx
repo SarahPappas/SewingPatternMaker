d55aff4983496987f5b9455afc7d90651b038481
Audrey Morin <34172843+msdrey@users.noreply.github.com>
2020-07-08 17:29:13 -0700

    added a basic upload image functionality

--- src/pages/AddPhoto/AddPhoto.tsx
+++ src/pages/AddPhoto/AddPhoto.tsx
@@ -1,8 +1,32 @@
-import React from 'react';
+import React, { ChangeEvent } from 'react';
 import './AddPhoto.css';
 
 export const AddPhoto: React.FC = () => {
+    const uploadedImage = React.useRef(document.createElement("img"));
+
+    const handleImageUpload = (e: ChangeEvent<HTMLInputElement>) => {
+        const fileList: FileList | null = e.target.files;
+        if (fileList) {
+            const reader = new FileReader();
+            reader.onload = () => {
+                if (reader.result) {
+                    uploadedImage.current.src = reader.result.toString();
+                }
+            };
+            reader.readAsDataURL(fileList[0]);
+        }
+    };
+    
     return (
-        <div className='addPhoto'>Add Photo page</div>
+        <div className='addPhoto'>
+            <h2>Add Photo page</h2>
+            <input type="file" accept="image/*" onChange={handleImageUpload} multiple={false}/>
+            <div style={{height: "200px",
+                        width: "200px",
+                        border: "2px dashed black"}}>
+                <img alt="your garment" style={{width: "100%",
+                                        position: "absolute"}} ref={uploadedImage}/>
+            </div>
+        </div>
     );
 };

e2613c4c57e39ca7a9ee42175e3e44282379bf1f
Sarah <pappas.sarah@gmail.com>
2020-07-09 11:01:54 -0600

    Update when PatternPathType comes from and change _path2D to not be optionally null

--- src/canvas/PatternPath.tsx
+++ src/canvas/PatternPath.tsx
@@ -1,17 +1,17 @@
 import { Point } from './Point';
-import { PatternPathType } from './PatternPathType';
+import { PatternPathType } from './Enums';
 
 export class PatternPath implements IPatternPath {
     private _points: Point[];
     private _type: PatternPathType;
-    private _pathCanvas2D: Path2D | null;
+    private _path2D: Path2D;
     private _isPathCanvas2DInvalid: boolean;
     private _lastIndexAddedToCanvas2DPath: number;
 
     constructor (pathType: PatternPathType) {
         this._type = pathType;
         this._points = new Array<Point>();
-        this._pathCanvas2D = null;
+        this._path2D = new Path2D();
         this._isPathCanvas2DInvalid = true;
         this._lastIndexAddedToCanvas2DPath = -1;
     }
@@ -24,26 +24,22 @@ export class PatternPath implements IPatternPath {
         return this._type;
     };
 
-    getPathCanvas2D = (): Path2D | null => {
-        if (!this._isPathCanvas2DInvalid) {
-            return this._pathCanvas2D;
+    getPathCanvas2D = (): Path2D => {
+        if (!this._isPathCanvas2DInvalid || !this._points.length) {
+            return this._path2D;
         }
 
-        if (!this._points.length || !this._pathCanvas2D) {
-            return null;
-        }
-
-        if (this._points.length == 1) {
-            this._pathCanvas2D.moveTo(this._points[0].getX(), this._points[0].getY());
+        if (this._points.length === 1) {
+            this._path2D.moveTo(this._points[0].getX(), this._points[0].getY());
         }
 
         const lastPointIndex = this._points.length - 1;
         for (let i = this._lastIndexAddedToCanvas2DPath + 1; i < this._points.length; i++) {
             const currPoint = this._points[i];
-            this._pathCanvas2D.lineTo(currPoint.getX(), currPoint.getY());
+            this._path2D.lineTo(currPoint.getX(), currPoint.getY());
         }
     
-        return this._pathCanvas2D;
+        return this._path2D;
     };
 
     addPoint = (point: Point): boolean => {

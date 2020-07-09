f7d0514b72af48a4143786c4495d9b00388977fe
Sarah <pappas.sarah@gmail.com>
2020-07-09 10:57:54 -0600

    Rename enum PatternPathName to PatternPathType and remove PatternPathColor enum because we added the map instead

--- src/canvas/Enums.tsx
+++ src/canvas/Enums.tsx
@@ -1,11 +1,5 @@
-export enum PatternPathName {
+export enum PatternPathType {
     Seam,
     Fold,
     Edge,
-}
-
-export enum PatternPathColor {
-    Blue = "#248EA6",
-    Green = "#84BFA4",
-    Yellow = "#F2E74B",
 }

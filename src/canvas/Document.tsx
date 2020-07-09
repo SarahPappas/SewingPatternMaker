8156803cc03250a555fb3bf79dc05ffdc1cf3ca3
Sarah <pappas.sarah@gmail.com>
2020-07-09 11:00:09 -0600

    Remove references to PatternPathType from Document, it no longer needs to keep the relationship between the type and the color because we are now using the map

--- src/canvas/Document.tsx
+++ src/canvas/Document.tsx
@@ -1,18 +1,10 @@
 import { PatternPath } from './PatternPath';
-import { PatternPathType } from './PatternPathType';
-import { PatternPathName, PatternPathColor } from './Enums';
 
 export class Document implements IDocument {
     private _patternPaths: PatternPath[];
-    patternPathType: {Fold: PatternPathType; Edge: PatternPathType; Seam: PatternPathType};
 
     constructor () {
         this._patternPaths = new Array<PatternPath>();
-        this.patternPathType = {
-            Fold: new PatternPathType(PatternPathName.Fold, PatternPathColor.Green),
-            Edge: new PatternPathType(PatternPathName.Edge, PatternPathColor.Yellow),
-            Seam: new PatternPathType(PatternPathName.Seam, PatternPathColor.Blue)
-        };
     }
 
     getPatternPaths = (): PatternPath[] => {
@@ -31,6 +23,6 @@ export class Document implements IDocument {
 
     // Removes the most recently added Pattern Path
     removePatternPath = (): boolean => {
-        return !!this._patternPaths.pop();
+        return Boolean(this._patternPaths.pop());
     }
 }

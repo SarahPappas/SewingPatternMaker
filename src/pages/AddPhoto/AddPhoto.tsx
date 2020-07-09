ca4256afa7b92be522ec2a75e72262cd2014059b
Audrey Morin <34172843+msdrey@users.noreply.github.com>
2020-07-09 11:06:29 -0700

    created a UploadPhoto component

--- src/pages/AddPhoto/AddPhoto.tsx
+++ src/pages/AddPhoto/AddPhoto.tsx
@@ -1,29 +1,12 @@
-import React, { ChangeEvent } from 'react';
+import React from 'react';
 import './AddPhoto.css';
+import { UploadPhoto } from 'components/UploadPhoto/UploadPhoto';
 
 export const AddPhoto: React.FC = () => {
-    const uploadedImage = React.useRef(document.createElement("img"));
-
-    const handleImageUpload = (e: ChangeEvent<HTMLInputElement>) => {
-        const fileList: FileList | null = e.target.files;
-        if (fileList) {
-            const reader = new FileReader();
-            reader.onload = () => {
-                if (reader.result) {
-                    uploadedImage.current.src = reader.result.toString();
-                }
-            };
-            reader.readAsDataURL(fileList[0]);
-        }
-    };
-    
     return (
         <div className='addPhoto'>
             <h2>Add Photo page</h2>
-            <input type="file" accept="image/*" onChange={handleImageUpload} multiple={false}/>
-            <div className='photoContainer'>
-                <img className='photo' alt="your garment" ref={uploadedImage}/>
-            </div>
+            <UploadPhoto></UploadPhoto>
         </div>
     );
 };

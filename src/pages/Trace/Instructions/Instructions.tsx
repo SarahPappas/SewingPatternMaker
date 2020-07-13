be7a4ac6a95d6f8b960dba8cd249ff14f82230ed
Sarah <pappas.sarah@gmail.com>
2020-07-13 16:32:47 -0600

    update Instructions page

--- src/pages/Trace/Instructions/Instructions.tsx
+++ src/pages/Trace/Instructions/Instructions.tsx
@@ -1,7 +1,17 @@
 import React from 'react';
+// import { ActionButton } from 'components/ActionButton/ActionButton';
+import { NavButton } from 'components/NavButton/NavButton';
+import { InstructionModal } from 'components/InstructionModal/InstructionModal';
+import './Instructions.css';
 
 export const Instructions: React.FC = () => {
-    return (
-        <div>Instructions</div>
-    );
+    const button: Button = {text: 'DONE', to: '/AddMeasurement' };
+    const instruction: InstructModal = {text: ['Choose seam, fold, or edge to add to your pattern']};
+
+    return (<>
+        <div className={'backgroundGrey'}></div>
+        <div className={'backgroundArrow'}></div>
+        <NavButton button={button}/> 
+        {/*<InstructionModal instructModal={instruction}/>*/}
+    </>);
 };

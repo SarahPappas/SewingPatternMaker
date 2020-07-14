625399bf665b6fef0e49d376aaee36f2cbcc44de
Sarah <pappas.sarah@gmail.com>
2020-07-14 14:32:32 -0600

    Input is not optional for InputButton component

--- src/components/InputButton/InputButton.tsx
+++ src/components/InputButton/InputButton.tsx
@@ -4,15 +4,15 @@ import './InputButton.css';
 
 interface InputButtonProps {
     button: ActionButton;
-    input?: Input;
+    input: Input;
 }
 
 export const InputButton: React.FC<InputButtonProps> = ({ button, input }) => {    
 
     return (<>
-        <input id={input?.id} type={input?.type} accept={input?.accept} onChange={button.action} multiple={false}/>
+        <input id={input.id} type={input.type} accept={input.accept} onChange={button.action} multiple={false}/>
 
-        <label htmlFor={input?.id} className={input?.className}> 
+        <label htmlFor={input.id} className={input.className}> 
         <Button label={button.label} className=''></Button>
         </label>
     </>);

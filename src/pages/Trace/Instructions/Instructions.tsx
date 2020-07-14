928aa9e8139a7ccd12f630b08e8281fd7be8cc73
Sarah <pappas.sarah@gmail.com>
2020-07-14 16:50:29 -0600

    Update Instructions to use new path type button grid componenet instead

--- src/pages/Trace/Instructions/Instructions.tsx
+++ src/pages/Trace/Instructions/Instructions.tsx
@@ -2,10 +2,11 @@ import React from 'react';
 // import { ActionButton } from 'components/ActionButton/ActionButton';
 import { NavButton } from 'components/NavButton/NavButton';
 import { InstructionModal } from 'components/InstructionModal/InstructionModal';
+import { PathTypeButtonGrid } from 'components/PathTypeButtonGrid/PathTypeButtonGrid';
 import './Instructions.css';
 
 export const Instructions: React.FC = () => {
-    const button: Button = {text: 'DONE', to: '/AddMeasurement' };
+    const button: NavButton = {label: 'DONE', to: '/AddMeasurement' };
     const instruction: InstructModal = {text: ['Choose seam, fold, or edge to add to your pattern']};
 
     return (<>
@@ -13,30 +14,7 @@ export const Instructions: React.FC = () => {
         <div className={'instructionsConatainer'}>
             <NavButton button={button}/> 
             <InstructionModal instructModal={instruction}/>
-            </div>
-            <div className="flexGrid">
-                <div className="col">
-                    <div className={'arrow'}></div>
-                    <div className={'seam'}>
-                        <div className={'colorBlock'}></div>
-                        <div className={'pathButtonLabel'}>Seam</div>
-                    </div>
-                    
-                </div>
-                <div className="col">
-                    <div className={'arrow'}></div>
-                    <div className={'fold'}>
-                        <div className={'colorBlock'}></div>
-                        <div className={'pathButtonLabel'}>Fold</div>
-                    </div>
-                </div>  
-                <div className="col">
-                    <div className={'arrow'}></div>
-                    <div className={'edge'}>
-                        <div className={'colorBlock'}></div>
-                        <div className={'pathButtonLabel'}>Edge</div>
-                    </div>
-                </div>
-            </div>
+        </div>
+        <PathTypeButtonGrid />
     </>);
 };

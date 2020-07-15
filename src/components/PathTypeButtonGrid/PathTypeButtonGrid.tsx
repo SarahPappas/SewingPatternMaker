42383797ae5afd641f73b38aac29ebf43db61d18
Sarah <pappas.sarah@gmail.com>
2020-07-15 13:44:24 -0600

    Remove arrows from PathTypeButtonGrid component

--- src/components/PathTypeButtonGrid/PathTypeButtonGrid.tsx
+++ src/components/PathTypeButtonGrid/PathTypeButtonGrid.tsx
@@ -6,21 +6,19 @@ import { PatternPathType } from 'canvas/Enums';
 interface PathTypeButtonGridProps {
     isEnabled: boolean;
     selectedType: PatternPathType;
+    to: string;
 }
 
-export const PathTypeButtonGrid: React.FC<PathTypeButtonGridProps> = ({isEnabled, selectedType}) => {
+export const PathTypeButtonGrid: React.FC<PathTypeButtonGridProps> = ({isEnabled, selectedType, to}) => {
     return (
         <div className="flexGrid">
             <div className="col">
-                <div className={'arrow'}></div>
                 <PathButton type={PatternPathType.Seam} isEnabled={isEnabled}/>
             </div>
             <div className="col">
-                <div className={'arrow'}></div>
                 <PathButton type={PatternPathType.Fold} isEnabled={isEnabled}/>
             </div>  
             <div className="col">
-                <div className={'arrow'}></div>
                 <PathButton type={PatternPathType.Edge} isEnabled={isEnabled}/>
             </div>
         </div>

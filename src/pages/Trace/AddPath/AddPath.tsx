9aab7557471fa8b552e01fe0953767541953a7ab
Sarah <pappas.sarah@gmail.com>
2020-07-15 16:26:53 -0600

    Add props that take the pathType and setPathType function in AddPath.tsx

--- src/pages/Trace/AddPath/AddPath.tsx
+++ src/pages/Trace/AddPath/AddPath.tsx
@@ -2,7 +2,12 @@ import React, { useRef, useEffect } from 'react';
 import { PathTypeButtonGrid } from 'components/PathTypeButtonGrid/PathTypeButtonGrid';
 import { PatternPathType } from 'canvas/Enums';
 
-export const AddPath: React.FC = () => {
+interface AddPathProps {
+    curPathType: PatternPathType;
+    setPathType: React.Dispatch<React.SetStateAction<PatternPathType>>;
+}
+
+export const AddPath: React.FC<AddPathProps> = ({curPathType, setPathType}) => {
     const canvasRef = useRef(document.getElementsByClassName('canvasContainer')[0]);
     useEffect(() => {
         canvasRef.current = document.getElementsByClassName('canvasContainer')[0];
@@ -10,6 +15,6 @@ export const AddPath: React.FC = () => {
     }, [canvasRef]);
 
     return (<>
-        <PathTypeButtonGrid isEnabled={false} selectedType={PatternPathType.UNDEFINED} to={''}/>
+        <PathTypeButtonGrid isEnabled={false} curPathType={curPathType} setPathType={setPathType}/>
     </>);
 };

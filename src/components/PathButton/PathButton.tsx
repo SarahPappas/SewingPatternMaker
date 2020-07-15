7afb228b90ad2a849afd7964daafbf9224d4966e
Sarah <pappas.sarah@gmail.com>
2020-07-15 16:31:45 -0600

    Set curPathType when button clicked and add notActive class to buttons that are not the current type

--- src/components/PathButton/PathButton.tsx
+++ src/components/PathButton/PathButton.tsx
@@ -6,12 +6,14 @@ import { PatternPathType } from 'canvas/Enums';
 interface PathButtonProps {
     type: PatternPathType;
     isEnabled: boolean;
+    curPathType: PatternPathType;
+    setPathType: React.Dispatch<React.SetStateAction<PatternPathType>>;
 }
 
-export const PathButton: React.FC<PathButtonProps> = ({type, isEnabled}) => {
+export const PathButton: React.FC<PathButtonProps> = ({type, isEnabled, curPathType, setPathType}) => {
     const typeName = PatternPathType[type];
     const className = typeName.toLowerCase();
-    const setPathType = new CustomEvent('setPathType', {
+    const setCanvasPathType = new CustomEvent('setPathType', {
         detail: { pathType: type }
     });
 
@@ -26,15 +28,19 @@ export const PathButton: React.FC<PathButtonProps> = ({type, isEnabled}) => {
             return;
         }
         
-        canvasRef.current?.dispatchEvent(setPathType);
+        setPathType(type);
+        canvasRef.current?.dispatchEvent(setCanvasPathType);
     };
+
+    let buttonLabelClass = 'pathButtonLabel';
+    buttonLabelClass = (curPathType && curPathType != type) ? buttonLabelClass + ' notActive' : buttonLabelClass;
 
     const buttonProps = {label: typeName, action: handleSetPathType};
     return (<>
         <ActionButton button={buttonProps}>
             <div className={className}>
-                <div className={'colorBlock'}></div>
-                <div className={'pathButtonLabel'}>{typeName}</div>
+                <div className='colorBlock'></div>
+                <div className={buttonLabelClass}>{typeName}</div>
             </div>
         </ActionButton>
     </>);

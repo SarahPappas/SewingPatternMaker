1be25fbe4bb98543fdb1d71645a50394fc53accd
Sarah <pappas.sarah@gmail.com>
2020-07-15 13:46:39 -0600

    Add arrows in separate flex grid in Instructions component. And remove import for unused ActionButton. Also, get canvas element as ref and dynamically add class to move canvas to background.

--- src/pages/Trace/Instructions/Instructions.tsx
+++ src/pages/Trace/Instructions/Instructions.tsx
@@ -1,5 +1,4 @@
-import React from 'react';
-// import { ActionButton } from 'components/ActionButton/ActionButton';
+import React, { useRef, useEffect } from 'react';
 import { NavButton } from 'components/NavButton/NavButton';
 import { InstructionModal } from 'components/InstructionModal/InstructionModal';
 import { PathTypeButtonGrid } from 'components/PathTypeButtonGrid/PathTypeButtonGrid';
@@ -7,16 +6,36 @@ import './Instructions.css';
 import { PatternPathType } from 'canvas/Enums';
 
 export const Instructions: React.FC = () => {
-    const button: NavButton = {label: 'DONE', to: '/AddMeasurement' };
+    const doneButton: NavButton = {label: 'DONE', to: '/AddMeasurement' };
+    const addPathButton: NavButton = {label:'', to: 'AddPath'};
     const instruction: InstructModal = {text: ['Choose seam, fold, or edge to add to your pattern']};
 
+    const canvasRef = useRef(document.getElementsByClassName('canvasContainer')[0]);
+    useEffect(() => {
+        canvasRef.current = document.getElementsByClassName('canvasContainer')[0];
+        canvasRef.current.classList.add('canvasContainerBackground');
+    }, [canvasRef]);
+    
     return (<>
         <div className={'backgroundGrey'}></div>
         <div className={'instructionsConatainer'}>
             {/* TODO: only show Done button when an enclosed shape exists */}
-            <NavButton button={button}/> 
+            <NavButton button={doneButton}/> 
             <InstructionModal instructModal={instruction}/>
         </div>
-        <PathTypeButtonGrid isEnabled={true} selectedType={PatternPathType.UNDEFINED}/>
+        <div className='arrowFlexGrid'>
+            <div className='arrowCol'>
+                <div className='arrow'></div>
+            </div>
+            <div className='arrowCol'>
+                <div className='arrow'></div>
+            </div>  
+            <div className='arrowCol'>
+                <div className='arrow'></div>
+            </div>
+        </div>
+        <NavButton button={addPathButton}>
+            <PathTypeButtonGrid isEnabled={true} selectedType={PatternPathType.UNDEFINED} to={''}/>
+        </NavButton>
     </>);
 };

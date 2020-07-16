377b55ff78ff160b84604fc7a7178c6ea7397251
Sarah <pappas.sarah@gmail.com>
2020-07-16 14:01:07 -0600

    Use new ToolButtonGrid in AddPath instead

--- src/pages/Trace/AddPath/AddPath.tsx
+++ src/pages/Trace/AddPath/AddPath.tsx
@@ -1,10 +1,7 @@
 import React, { useRef, useEffect } from 'react';
 import { PathTypeButtonGrid } from 'components/PathTypeButtonGrid/PathTypeButtonGrid';
 import { PatternPathType } from 'canvas/Enums';
-import { ReactComponent as FreeLineToolIcon } from '../../../assets/free-line-tool.svg';
-import { ReactComponent as StraightLineToolIcon } from '../../../assets/straight-line-tool.svg';
-import { PatternPathColor } from 'canvas/PatternPathColor';
-import './AddPath.css';
+import{ ToolButtonGrid } from 'components/ToolButtonGrid/ToolButtonGrid';
 
 interface AddPathProps {
     curPathType: PatternPathType;
@@ -18,31 +15,8 @@ export const AddPath: React.FC<AddPathProps> = ({curPathType, setPathType}) => {
         canvasRef.current.classList.remove('canvasContainerBackground');
     }, [canvasRef]);
 
-    // Sets the correct color for the svg path icon 
-    let iconStyle = {};
-    if (curPathType) {
-        iconStyle = {
-            stroke: PatternPathColor.get(curPathType)
-        };
-    }
-    
-    // Do not show free line tool if path type is a fold.
-    let freeLineTool = <></>;
-    if (curPathType !== PatternPathType.Fold) {
-        freeLineTool = <>
-            <div className='toolContainer'>
-                <FreeLineToolIcon style={iconStyle} />
-            </div>
-        </>;
-    }
-
     return (<>
-        <div className='toolBar'>
-            <div className='toolContainer' >
-                <StraightLineToolIcon style={iconStyle} />
-            </div>
-            {freeLineTool}
-        </div>
+        <ToolButtonGrid curPathType={curPathType}/>
         <PathTypeButtonGrid isEnabled={false} curPathType={curPathType} setPathType={setPathType}/>
     </>);
 };

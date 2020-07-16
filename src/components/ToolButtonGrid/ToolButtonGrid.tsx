2a2f3c293ef3b1f62b31c284beaa49a45147c831
Sarah <pappas.sarah@gmail.com>
2020-07-16 17:57:52 -0600

    update ToolButtonGrid to use ToolButton Component

--- src/components/ToolButtonGrid/ToolButtonGrid.tsx
+++ src/components/ToolButtonGrid/ToolButtonGrid.tsx
@@ -1,54 +1,39 @@
 import React, { useRef, useEffect } from 'react';
-import { PatternPathType } from 'canvas/Enums';
-import { ReactComponent as FreeLineToolIcon } from '../../../assets/free-line-tool.svg';
-import { ReactComponent as StraightLineToolIcon } from '../../../assets/straight-line-tool.svg';
-import { PatternPathColor } from 'canvas/PatternPathColor';
-import { ActionButton } from 'components/ActionButton/ActionButton';
-import './AddPath.css';
+import { PatternPathType, ToolType } from 'canvas/Enums';
+import { ReactComponent as FreeLineToolIcon } from '../../assets/free-line-tool.svg';
+import { ReactComponent as StraightLineToolIcon } from '../../assets/straight-line-tool.svg';
+import {PatternPathColor} from 'canvas/PatternPathColor';
+import { ToolButton } from 'components/ToolButton/ToolButton';
+import './ToolButtonGrid.css';
 
 interface ToolButtonGridProps {
     curPathType: PatternPathType;
 }
 
 export const ToolButtonGrid: React.FC<ToolButtonGridProps> = ({curPathType}) => {
-    const canvasRef = useRef(document.getElementsByClassName('canvasContainer')[0]);
-    useEffect(() => {
-        canvasRef.current = document.getElementsByClassName('canvasContainer')[0];
-    }, [canvasRef]);
+    // By default the line tool should be selected
+    const toolType = ToolType.StraightLine;
 
-    // Sets the correct color for the svg path icon.
-    let iconStyle = {};
-    if (curPathType) {
-        iconStyle = {
-            stroke: PatternPathColor.get(curPathType)
-        };
-    }
-
-    // Setup props for action button.
-    const handleSetTool = () => {
-
-    };
-
-    let button = {label:"", action:handleSetTool};
-    
+    const selectedType = ToolType.StraightLine;
+        
     // Do not show free line tool if path type is a fold.
     let freeLineTool = <></>;
+    const iconStyle = {
+        stroke: PatternPathColor.get(curPathType)
+    };
+
     if (curPathType !== PatternPathType.Fold) {
         freeLineTool =
-            <ActionButton button={button}>
-                <div className='toolContainer'>
-                    <FreeLineToolIcon style={iconStyle} />
-                </div>
-            </ActionButton>;
+            <ToolButton curPathType={curPathType} toolType={ToolType.Freeline} selectedType={selectedType}>
+                <FreeLineToolIcon style={iconStyle}></FreeLineToolIcon>
+            </ToolButton>;
     }
 
     return (
         <div className='toolBar'>
-            <ActionButton button={button}>
-                <div className='toolContainer' >
-                    <StraightLineToolIcon style={iconStyle} />
-                </div>
-            </ActionButton>  
+            <ToolButton curPathType={curPathType} toolType={ToolType.StraightLine} selectedType={selectedType}>
+                <StraightLineToolIcon style={iconStyle}></StraightLineToolIcon>
+            </ToolButton>
             {freeLineTool}
         </div>
     );

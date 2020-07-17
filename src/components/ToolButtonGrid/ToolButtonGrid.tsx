0bc11f3cf5ceebc9bd978953d50a1d5fe4e2693f
Sarah <pappas.sarah@gmail.com>
2020-07-17 14:08:55 -0600

    Fix spacing for ToolButton and rearange code, so that freeLineTool is next to the code that usees it.

--- src/components/ToolButtonGrid/ToolButtonGrid.tsx
+++ src/components/ToolButtonGrid/ToolButtonGrid.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 import { PatternPathType, ToolType } from 'canvas/Enums';
 import { ReactComponent as FreeLineToolIcon } from '../../assets/free-line-tool.svg';
 import { ReactComponent as StraightLineToolIcon } from '../../assets/straight-line-tool.svg';
-import {PatternPathColor} from 'canvas/PatternPathColor';
+import { PatternPathColor} from 'canvas/PatternPathColor';
 import { ToolButton } from 'components/ToolButton/ToolButton';
 import './ToolButtonGrid.css';
 
@@ -13,13 +13,14 @@ interface ToolButtonGridProps {
 export const ToolButtonGrid: React.FC<ToolButtonGridProps> = ({curPathType}) => {
     // By default the line tool should be selected
     const [selectedToolType, setSelectedToolType] = React.useState<ToolType>(ToolType.StraightLine);
-        
-    // Do not show free line tool if path type is a fold.
-    let freeLineTool = <></>;
+    
+    // Set the icon color.
     const iconStyle = {
         stroke: PatternPathColor.get(curPathType)
     };
 
+    // Do not show free line tool if path type is a fold.
+    let freeLineTool = <></>;
     if (curPathType !== PatternPathType.Fold) {
         freeLineTool =
             <ToolButton toolType={ToolType.Freeline} selectedType={selectedToolType} setSelectedType={setSelectedToolType}>

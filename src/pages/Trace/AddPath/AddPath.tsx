b62d4eab8e62713699e4008a9bbf8633d799fd35
Sarah <pappas.sarah@gmail.com>
2020-07-17 11:36:03 -0600

    Add an event listner to canvas to change pages when a path is complete

--- src/pages/Trace/AddPath/AddPath.tsx
+++ src/pages/Trace/AddPath/AddPath.tsx
@@ -1,4 +1,5 @@
-import React from 'react';
+import React, { useEffect, useRef } from 'react';
+import { useHistory } from "react-router-dom";
 import { PathTypeButtonGrid } from 'components/PathTypeButtonGrid/PathTypeButtonGrid';
 import { PatternPathType } from 'canvas/Enums';
 import{ ToolButtonGrid } from 'components/ToolButtonGrid/ToolButtonGrid';
@@ -9,6 +10,16 @@ interface AddPathProps {
 }
 
 export const AddPath: React.FC<AddPathProps> = ({curPathType, setPathType}) => {
+    const canvasRef = useRef(document.querySelector('canvas'));
+    useEffect(() => {
+        canvasRef.current = document.querySelector('canvas');
+    }, [canvasRef]);
+    
+    const history = useHistory();
+
+    canvasRef.current?.addEventListener("endPath", () => {
+        history.push('/Review');
+    });
 
     return (<>
         <ToolButtonGrid curPathType={curPathType}/>

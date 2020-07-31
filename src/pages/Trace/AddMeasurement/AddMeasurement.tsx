23a91cad62b1c551a44180458b7f0dd40c957b04
Audrey Morin <34172843+msdrey@users.noreply.github.com>
2020-07-31 11:11:16 -0700

    modify css of instructions on addmeasurement so that its out of the way of the canvas

--- src/pages/Trace/AddMeasurement/AddMeasurement.tsx
+++ src/pages/Trace/AddMeasurement/AddMeasurement.tsx
@@ -1,6 +1,7 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useRef } from 'react';
 import { InstructionModal } from 'components/InstructionModal/InstructionModal';
 import './AddMeasurement.css';
+import { useHistory } from 'react-router-dom';
 
 interface AddMeasurementProps {
     setUploadedFileData: React.Dispatch<React.SetStateAction<string>>;
@@ -9,14 +10,34 @@ interface AddMeasurementProps {
 export const AddMeasurement: React.FC<AddMeasurementProps> = ({ setUploadedFileData }) => {
     const instructModal: InstructModal = {text: ['Choose a path to measure.']};
     
+    // Remove class that puts canvas in the background.
+    const canvasContainerRef = useRef(document.getElementsByClassName('canvasContainer')[0]);
+    useEffect(() => {
+        canvasContainerRef.current = document.getElementsByClassName('canvasContainer')[0];
+        canvasContainerRef.current.classList.remove('canvasContainerBackground');
+    }, [canvasContainerRef]);
+
+
     //const canvasContainerRef = useRef(document.querySelector<HTMLElement>('canvasContainer'));
     useEffect(() => {
         setUploadedFileData("");
     }, [setUploadedFileData]);
 
+    // Get the Canvas so that we can and an event listener to it.
+    const canvasRef = useRef(document.querySelector('canvas'));
+    useEffect(() => {
+        canvasRef.current = document.querySelector('canvas');
+    }, [canvasRef]);
+    
+    const history = useHistory();
+
+    canvasRef.current?.addEventListener("endTracing", () => {
+        history.push('/Trace/Review');
+    });
+
     return (
         <>
-            <div className={'instructionsContainer'}>
+            <div className={'measurementInstructionsContainer'}>
                 <InstructionModal instructModal={instructModal}></InstructionModal>
             </div>
             <div>

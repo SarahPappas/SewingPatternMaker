edbd12552f844fbcca7b917a6df04b47273b2be9
Sarah <pappas.sarah@gmail.com>
2020-07-31 15:14:29 -0700

    Remove functions related to spliting the path, move clostestDistnanceSquared to the Point class, remove import for direction enum, make Fit function static.

--- src/canvas/CurveFitter.tsx
+++ src/canvas/CurveFitter.tsx
@@ -1,17 +1,9 @@
 import { Point } from './Point';
 import { Curve } from './Curve';
-import { Direction } from './Enums';
 
 export class CurveFitter {
-    private _points: Point[];
-    private _splitsIndex: number[];
-    
-    constructor (points: Point[]) {
-        this._points = points;
-        this._splitsIndex = new Array<number>();
-    }
 
-    Fit = (points: Point[]): Curve => {
+    static Fit = (points: Point[]): Curve => {
         // If there are two points or less, return void becuase this cannnot be fit to a curve.
         if (points.length <= 2) {
             throw new Error("not enough points");
@@ -74,7 +66,7 @@ export class CurveFitter {
 
                 let maxDelta = 0;
                 for (let i = 0;i < numPointsOnPotentialcurve;i++) {
-                    const delta = this.closestDistanceSquared(potentialCurvePoints[i], points);
+                    const delta = potentialCurvePoints[i].closestDistanceSquaredFromSetOfPoints(points);
                     if (delta > maxDelta) {
                         maxDelta = delta; 
                     }
@@ -90,89 +82,5 @@ export class CurveFitter {
 
         return bestCurve;
     };
-
-    private closestDistanceSquared = (point: Point, points: Point[]): number => {
-        let minDistance = Number.MAX_VALUE;
-
-
-        for (let i = 0;i < points.length;i++) {
-            const distance = point.distanceSquared(points[i]);
-            if (distance < minDistance) {
-                minDistance = distance;
-            }
-        }
-
-        return minDistance;
-    }
-
-    private splitPathBySlope = (points: Point[]): void => {
-        if (points.length < 3) {
-            return;
-        }
-
-        let startIndex = 0;
-        let prevSlope = this.getSlope(points[startIndex], points[1]);
-        let nextSlope;
-        let direction = Direction.UNDEFINED;
-        const SLOPE_THRESHOLD = 1;
-        const INDEX_THRESHOLD = 3;
-
-        // Use every other point to calculate slope
-        for (let i = 1; i < points.length - 5; i++) {
-            nextSlope = this.getSlope(points[startIndex], points[i]);
-
-            // If either the previous slope or the next slope is infiinty, skip this iteration of the loop.
-            if (prevSlope === Infinity) {
-                prevSlope = nextSlope;
-                continue;
-            }
-
-            if (nextSlope === Infinity) {
-                continue;
-            }
-
-            // If the direction is the the slope is moving is not defined, set the direction.
-            if (!direction) {
-                if (prevSlope - nextSlope > 0) {
-                    direction = Direction.Decrease;
-                } else if (prevSlope - nextSlope < 0) {
-                    direction = Direction.Increase;
-                }
-            }
-
-            switch (direction) {
-                case Direction.Increase: 
-                    /* If the difference in slope is greater than the slope threshold and there are not splits yet, add a split. 
-                     * Otherwise, if the difference in slope is greater than the slope threshold and the difference in index is 
-                     * greater than the index threshold, add the split.
-                     */
-                    if ((prevSlope - nextSlope > SLOPE_THRESHOLD && !this._splitsIndex.length) 
-                        || (prevSlope - nextSlope > SLOPE_THRESHOLD && i - this._splitsIndex[this._splitsIndex.length - 1] > INDEX_THRESHOLD)) {
-                        this._splitsIndex.push(i);
-                        // Set the start index to the current index.
-                        startIndex = i;
-                        // Reset the direction/
-                        direction = Direction.UNDEFINED;
-                    }
-                    break;
-                case Direction.Decrease:
-                    /* If the difference in slope is less than the slope threshold and there are not splits yet, add a split. 
-                     * Otherwise, if the difference in slope is less than the slope threshold and the difference in index is 
-                     * greater than the index threshold, add the split.
-                     */
-                    if ((prevSlope - nextSlope < -SLOPE_THRESHOLD  && !this._splitsIndex.length) 
-                        || (prevSlope - nextSlope < -SLOPE_THRESHOLD && i - this._splitsIndex[this._splitsIndex.length - 1] > INDEX_THRESHOLD)) {
-                        this._splitsIndex.push(i);
-                        // Set the start index to the current index.
-                        startIndex =  i;
-                        // Reset the direction.
-                        direction = Direction.UNDEFINED;
-                    }
-                    break;
-            }
-
-            prevSlope = nextSlope;
-        }
-    }
 }
 

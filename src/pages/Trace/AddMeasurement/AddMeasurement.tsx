a8b62dd8508df731fb4815166417cfd404e50d13
Audrey Morin <34172843+msdrey@users.noreply.github.com>
2020-07-31 10:03:44 -0700

    created a new measurement page

---
+++ src/pages/Trace/AddMeasurement/AddMeasurement.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import { Button } from 'components/Button/Button';
+
+export const AddMeasurement: React.FC = () => {
+    return (
+        <>
+            <Button label='Choose a path to measure.' className='button'></Button>
+        </>
+    );
+};

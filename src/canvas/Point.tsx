5b8870e894d72812308c22643936c6f5b00d943a
Sarah <pappas.sarah@gmail.com>
2020-08-03 14:18:20 -0700

    Fix semi colon spacing in Point

--- src/canvas/Point.tsx
+++ src/canvas/Point.tsx
@@ -28,7 +28,7 @@ export class Point implements IPoint {
     closestDistanceSquaredFromSetOfPoints = (points: Point[]): number => {
         let minDistance = Number.MAX_VALUE;
 
-        for (let i = 0;i < points.length;i++) {
+        for (let i = 0; i < points.length; i++) {
             const distance = this.distanceSquared(points[i]);
             if (distance < minDistance) {
                 minDistance = distance;

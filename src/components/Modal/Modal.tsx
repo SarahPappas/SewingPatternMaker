1d19c9d4a812357bb117f129aaa8055d36709cd3
Sarah <pappas.sarah@gmail.com>
2020-08-06 14:47:55 -0700

    change name of InstructionModal to Modal

---
+++ src/components/Modal/Modal.tsx
@@ -0,0 +1,31 @@
+import React from 'react';
+import './Modal.css';
+import { ModalType } from 'canvas/Enums';
+
+interface ModalProps {
+    modal: Modal;
+}
+
+export const Modal: React.FC<ModalProps> = ({ modal }) => {
+    let itemClassName: string , modalClassName: string;
+    switch(modal.type) {
+        case ModalType.Instruction:
+            itemClassName = 'instructionItem';
+            modalClassName = 'instructionModal';
+            break;
+        case ModalType.Warning:
+            itemClassName = 'warningItem';
+            modalClassName = 'warningModal';
+            break;
+    }
+
+    const messages = modal.text.map((message, i) =>
+        <div className={itemClassName} key={i}>{message}</div>
+    );
+    
+    return (<>
+        <div className={modalClassName}>
+            {messages}
+        </div>
+    </>);
+};

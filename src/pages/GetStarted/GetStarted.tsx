ee40c22bd3c8e6f3d7d117fa5d7aac9c3b0a86f6
Sarah <pappas.sarah@gmail.com>
2020-08-06 15:16:31 -0700

    use Modal instead of Instruction Modal in GetStarted

--- src/pages/GetStarted/GetStarted.tsx
+++ src/pages/GetStarted/GetStarted.tsx
@@ -1,19 +1,20 @@
 import React from 'react';
 import { NavButton } from 'components/NavButton/NavButton';
-import { InstructionModal } from 'components/InstructionModal/InstructionModal';
+import { Modal as InstructionModal } from 'components/Modal/Modal';
 import backgroundArrow from '../../assets/background-arrow-grey.svg';
 import './GetStarted.css';
+import { ModalType } from 'canvas/Enums';
 
 const button: NavButton = {label: "GET STARTED", to: "/AddPhoto" };
-const intro: InstructModal = {text: ["Let’s Make a \npattern in 3 \nsteps"]};
-const steps: InstructModal = {text: ["1. Upload photo", "2. Trace pattern", "3. Print pdf"]};
+const intro: Modal = {text: ["Let’s Make a \npattern in 3 \nsteps"], type: ModalType.Instruction};
+const steps: Modal = {text: ["1. Upload photo", "2. Trace pattern", "3. Print pdf"], type: ModalType.Instruction};
 
 export const GetStarted: React.FC = () => {
     return (<>
         <div className={'backgroundImageTop'}></div>
         <img className={'backgroundImageBottom'} src={backgroundArrow} alt='down arrow'></img>
-        <InstructionModal instructModal={intro}/>
-        <div className={'introSteps'}><InstructionModal instructModal={steps}/></div>
+        <InstructionModal modal={intro}/>
+        <div className={'introSteps'}><InstructionModal modal={steps}/></div>
         <NavButton button={button}/> 
     </>);
 };

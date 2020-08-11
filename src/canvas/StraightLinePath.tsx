60c7637a16134f2e698f08b4524bb6a2ba802def
Sarah <pappas.sarah@gmail.com>
2020-08-11 15:21:28 -0700

    Add missing semicolons

--- src/canvas/StraightLinePath.tsx
+++ src/canvas/StraightLinePath.tsx
@@ -8,7 +8,7 @@ export class StraightLinePath extends PatternPath {
 
     getLengthInPixels = (): number => {
         return Math.sqrt(this._points[0].distanceSquared(this._points[this._points.length - 1]));
-    }
+    };
 
     protected _updatePath2D = (): void => {
         const firstPoint = this._points[0];
@@ -18,5 +18,5 @@ export class StraightLinePath extends PatternPath {
         this._isPath2DValid = true;
         this._path2D.moveTo(firstPoint.getX(), firstPoint.getY());
         this._path2D.lineTo(lastPoint.getX(), lastPoint.getY());
-    }
+    };
 }

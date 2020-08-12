289b52e9ca43390e84a75d951c0d170f0a3d0b3a
Sarah <pappas.sarah@gmail.com>
2020-08-12 16:21:40 -0700

    Further fix enclosed path checking

--- src/canvas/Document.tsx
+++ src/canvas/Document.tsx
@@ -48,9 +48,11 @@ export class Document implements IDocument {
                 continue;
             }
 
-            for (let j = i + 1; j < endpoints.length; j++) {
+            // Check if end point matches any other endpoint.
+            for (let j = 0; j < endpoints.length && !endpoints[i].matched; j++) {
                 const o = endpoints[j];
-                if(!o) {
+
+                if (o === endpoints[i]) {
                     continue;
                 }
 
@@ -59,6 +61,8 @@ export class Document implements IDocument {
                     endpoints[j].matched = true;
                 }
             }
+
+            // TODO: Check if end points are on line;
 
             if (!endpoints[i].matched) {
                 return false;

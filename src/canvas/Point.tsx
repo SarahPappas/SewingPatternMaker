932312575f156662ed827c851f1fcb8706227bd5
Sarah <pappas.sarah@gmail.com>
2020-08-11 20:20:21 -0700

    Add missing semicolons

--- src/canvas/Point.tsx
+++ src/canvas/Point.tsx
@@ -9,25 +9,25 @@ export class Point implements IPoint {
 
     getX = (): number => {
         return this._x;
-    }
+    };
 
     getY = (): number => {
         return this._y;
-    }
+    };
 
     equals = (o: Point): boolean => {
         return this._x === o.getX() && this._y === o.getY();
-    }
+    };
 
     isWithinRadius = (o: Point, radius: number): boolean => {
         return this.distanceSquared(o) < radius * radius;
-    }
+    };
     
     distanceSquared = (point: Point): number => {
         const dx = this._x - point.getX();
         const dy = this._y - point.getY();
         return dx * dx + dy * dy;
-    }
+    };
 
     closestDistanceSquaredFromSetOfPoints = (points: Point[]): number => {
         let minDistance = Number.MAX_VALUE;
@@ -40,12 +40,12 @@ export class Point implements IPoint {
         }
 
         return minDistance;
-    }
+    };
 
     computeMiddlePoint = (point2: Point): Point => {
         const middleX = (this.getX() + point2.getX()) / 2;
         const middleY = (this.getY() + point2.getY()) / 2;
         
         return new Point(middleX, middleY);
-    }
+    };
 }

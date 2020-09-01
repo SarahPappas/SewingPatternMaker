ca7c1fd0da3c45c8c401d3aa9bdf6a49e58f88a1
Audrey Morin <34172843+msdrey@users.noreply.github.com>
2020-09-01 11:18:32 -0700

    fixed typo in LineSegment findIntersectionPointOfTwoLines

--- src/canvas/Geometry/LineSegment.tsx
+++ src/canvas/Geometry/LineSegment.tsx
@@ -79,7 +79,7 @@ export class LineSegment extends Segment {
     /**
      * Finds an intersection point of two lines. If withinSegment is false, it 
      * will return the intersection point between the lines that support 
-     * the two inputted lines regardless of where that point is, and return null
+     * the two inputted lines segments regardless of where that point is, and return null
      * if the lines are parallel.
      * If withinSegment is true, it will only return the point of intersection
      * if that point belongs to both of the inputted segments, and null

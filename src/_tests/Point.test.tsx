853fd3fc0e63f3cf10624da3382a36e12efe04d7
Audrey Morin <34172843+msdrey@users.noreply.github.com>
2020-09-02 17:52:59 -0700

    stress tested Point's .equal method and adjusted the epsilon

--- src/_tests/Point.test.tsx
+++ src/_tests/Point.test.tsx
@@ -6,4 +6,10 @@ test("clone", async () => {
 
     expect(p2.equals(p1)).toBeTruthy();
     expect(p2 === p1).toBeFalsy();
+
+    const p3 = new Point(Math.PI, Math.sqrt(2));
+    const p4 = new Point(Math.PI * Math.sqrt(2) * Math.sqrt(2) / 2, Math.sqrt(2) * Math.PI / Math.PI);
+
+    expect(p3.equals(p4)).toBeTruthy();
+    expect(p3 === p4).toBeFalsy();
 });

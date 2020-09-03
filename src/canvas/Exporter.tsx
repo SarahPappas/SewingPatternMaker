f8e3052ff7e48c553e9e791d302fd63c710eab24
Sarah <pappas.sarah@gmail.com>
2020-09-03 15:36:15 -0700

    Exporter takes patterPieces as parameter and loops through paths to draw

--- src/canvas/Exporter.tsx
+++ src/canvas/Exporter.tsx
@@ -1,14 +1,26 @@
 import { jsPDF } from 'jspdf';
+import { PatternPath } from './PatternPaths/PatternPath';
 
 export class Exporter {
     doc: jsPDF;
+    protected patternPieces: PatternPath[][];
 
-    constructor () {
+    constructor (patternPieces: PatternPath[][]) {
         this.doc = new jsPDF();
-        this.doc.text("Hello world!", 10, 10);
+        this.patternPieces = patternPieces;
+        
     }
 
     save = () => {
+        this.patternPieces.forEach(patternPiece => {
+            patternPiece.forEach(patternPath => {
+                // const pathStart = patternPath.get
+                // this.doc.moveTo()
+            });
+        });
+
         this.doc.save("test.pdf");
     };
+
+
 }

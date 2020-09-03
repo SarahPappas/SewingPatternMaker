49377f53c4213a98bde552d4807f758154e7ac8c
Sarah <pappas.sarah@gmail.com>
2020-09-03 14:55:18 -0700

    Move final review page out of trace path

---
+++ src/pages/FinalReview/FinalReview.tsx
@@ -0,0 +1,21 @@
+import React, { useEffect } from "react";
+import { App } from "canvas/AppController";
+import { ActionButton } from "components/ActionButton/ActionButton";
+
+export const FinalReview: React.FC = () => {
+    useEffect(() => {
+        App.renderer.finalReviewInit();
+    }, []);
+    
+    const handleExport = (): void => {
+        console.log('export');
+    };
+
+    const exportButton: Button = {label: 'EXPORT PDF', className: 'navButton'};
+    return (<>
+        <div>
+            Final review pages
+        </div>
+        <ActionButton button={exportButton} action={handleExport} />
+    </>);
+};

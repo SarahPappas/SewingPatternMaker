5d2a1697dfdee60b70acc09fb75588a3d7cd7925
Sarah <pappas.sarah@gmail.com>
2020-09-03 14:37:51 -0700

    Add Exporter to AppControler

--- src/canvas/AppController.tsx
+++ src/canvas/AppController.tsx
@@ -1,16 +1,19 @@
 import { Renderer } from './Renderer';
 import { Document } from './Document';
 import { PathSelection } from './PathSelection';
+import { Exporter } from './Exporter';
 
 class AppController {
     renderer: Renderer;
     document: Document;
     pathSelection: PathSelection;
+    exporter: Exporter;
 
     constructor () {
         this.document = new Document();
         this.pathSelection = new PathSelection();
         this.renderer = new Renderer(this.document, this.pathSelection);
+        this.exporter = new Exporter();
     }
 }
 

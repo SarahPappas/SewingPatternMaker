e21f739eb3943cc0194099f86a7123a547760083
Sarah <pappas.sarah@gmail.com>
2020-09-03 14:37:32 -0700

    Get started with jspdf

--- src/canvas/Exporter.tsx
+++ src/canvas/Exporter.tsx
@@ -1,3 +1,15 @@
+import { jsPDF } from 'jspdf';
+
 export class Exporter {
-    
+    doc: jsPDF;
+
+    constructor () {
+        this.doc = new jsPDF();
+        this.doc.text("Hello world!", 10, 10);
+        this.doc.save("a4.pdf");
+    }
+
+    save = () => {
+        this.doc.save("test.pdf");
+    };
 }

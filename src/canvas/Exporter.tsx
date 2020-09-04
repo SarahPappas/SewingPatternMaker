1f910dc70fe0fc99d151b1716f7a9be52c362534
Sarah <pappas.sarah@gmail.com>
2020-09-04 10:50:49 -0700

    Use jspdf's context2D to drawsegments. Get pattern pieces on save.

--- src/canvas/Exporter.tsx
+++ src/canvas/Exporter.tsx
@@ -1,26 +1,35 @@
 import { jsPDF } from 'jspdf';
+import { Document } from './Document';
 import { PatternPath } from './PatternPaths/PatternPath';
 
 export class Exporter {
     doc: jsPDF;
-    protected patternPieces: PatternPath[][];
+    protected _documentModel: Document;
+    protected _patternPieces: PatternPath[][];
 
-    constructor (patternPieces: PatternPath[][]) {
-        this.doc = new jsPDF();
-        this.patternPieces = patternPieces;
-        
+    constructor (documentModel: Document) {
+        this.doc = new jsPDF('p', 'pt', 'letter');
+        this._documentModel = documentModel;
+        this._patternPieces = [];
     }
 
     save = () => {
-        this.patternPieces.forEach(patternPiece => {
+        this._patternPieces = this._documentModel.getPatternPieces();
+        const ctx = this.doc.context2d;
+        ctx.strokeStyle = '#e605c4';
+        this._patternPieces.forEach(patternPiece => {
             patternPiece.forEach(patternPath => {
-                // const pathStart = patternPath.get
-                // this.doc.moveTo()
-            });
+                const pathStart = patternPath.getStart();
+                ctx.moveTo(pathStart.x, pathStart.y);
+                const segments = patternPath.getSegments();
+                segments.forEach(segment => {
+                    segment.drawTo(ctx);
+                });
+             });
         });
+
+        ctx.stroke();
 
         this.doc.save("test.pdf");
     };
-
-
 }

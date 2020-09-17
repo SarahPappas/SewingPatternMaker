0c6b1372ee5ff3b9b1cfed275c75f961219bc56a
Sarah <pappas.sarah@gmail.com>
2020-09-17 15:44:35 -0700

    Add setTimeout for save, so that react can  render the spinner

--- src/pages/FinalReview/FinalReview.tsx
+++ src/pages/FinalReview/FinalReview.tsx
@@ -1,10 +1,30 @@
-import React from "react";
-import { App } from "canvas/AppController";
-import { ActionButton } from "components/ActionButton/ActionButton";
+import React, { useEffect }    from 'react';
+import { App } from 'canvas/AppController';
+import { ActionButton } from 'components/ActionButton/ActionButton';
+
+import './FinalReview.css';
 
 export const FinalReview: React.FC = () => {
-    const handleExport = (): void => {
-        App.exporter.save();
+    const [loading, setLoading] = React.useState<boolean>(false);
+
+    // Generating the pdf takes time, so we are using an async call to not block the main js thread.
+    // let handleExport: Promise<unknown>;
+    let loader;
+    if (loading) {
+        console.log("loading is true");
+        loader = <div className={'lds-hourglass'}/>;
+    } else {
+        console.log("loading is false");
+        loader = <div />;
+    }
+    
+    const handleClick = () => {
+        setLoading(true);
+
+        setTimeout(() => {
+            App.exporter.save();
+            setLoading(false);
+        }, 0);
     };
 
     const exportButton: Button = {label: 'EXPORT PDF', className: 'navButton'};
@@ -12,6 +32,8 @@ export const FinalReview: React.FC = () => {
         <div>
             Final review pages
         </div>
-        <ActionButton button={exportButton} action={handleExport} />
+        <div id={'download-pattern'} />
+        {loader}
+        <ActionButton button={exportButton} action={handleClick} />
     </>);
 };

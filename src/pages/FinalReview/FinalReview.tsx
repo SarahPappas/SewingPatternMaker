6f8dfc42b19f03dfcfa941d88226d8c843a1b2aa
Sarah <pappas.sarah@gmail.com>
2020-09-17 19:15:39 -0700

    Update final review page to remove export button and show thanks message after download

--- src/pages/FinalReview/FinalReview.tsx
+++ src/pages/FinalReview/FinalReview.tsx
@@ -1,24 +1,30 @@
-import React, { useEffect }    from 'react';
+import React from 'react';
 import { App } from 'canvas/AppController';
 import { ActionButton } from 'components/ActionButton/ActionButton';
-
+import { Modal } from 'components/Modal/Modal';
+import { ModalType } from 'canvas/Enums';
 import './FinalReview.css';
 
 export const FinalReview: React.FC = () => {
     const [loading, setLoading] = React.useState<boolean>(false);
+    const [showExportButton, setExport] = React.useState<boolean>(true);
 
     // Generating the pdf takes time, so we are using an async call to not block the main js thread.
-    // let handleExport: Promise<unknown>;
     let loader;
     if (loading) {
-        console.log("loading is true");
         loader = <div className={'lds-hourglass'}/>;
+        
     } else {
-        console.log("loading is false");
         loader = <div />;
     }
     
     const handleClick = () => {
+        setExport(false);
+        console.log("loading? ", loading);
+        if (loading) {
+            return;
+        }
+
         setLoading(true);
 
         setTimeout(() => {
@@ -28,12 +34,16 @@ export const FinalReview: React.FC = () => {
     };
 
     const exportButton: Button = {label: 'EXPORT PDF', className: 'navButton'};
+    const thanksMessage: Modal = {text: ['Your pattern has been downloaded'], type: ModalType.Instruction};
     return (<>
         <div>
-            Final review pages
-        </div>
-        <div id={'download-pattern'} />
-        {loader}
-        <ActionButton button={exportButton} action={handleClick} />
+            {loader}
+            <div style={{display: showExportButton ? 'block' : 'none'}}>
+                <ActionButton button={exportButton} action={handleClick} />
+            </div>
+            <div style={{display: (!showExportButton  && !loading) ? 'block' : 'none'}}>
+                <Modal  modal={thanksMessage}/>
+            </div>
+        </div> 
     </>);
 };

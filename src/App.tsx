efa4679afb983eb70ea567ad1746ee11fe90c856
SarahPappas <pappas.sarah@gmail.com>
2020-12-04 13:54:42 -0600

    Changes for S3 (#51)
    * remove react logo files
    
    * remove icons
    
    * add homepage for aws static hosting
    
    * need to use HashRouter instead of BrowserRouter for static hosting
    
    * change name Bobbin Lab to BobbinLab
    
    * change name Bobbin Lab to BobbinLab in manifest

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { Main } from 'pages/Main/Main';
-import { BrowserRouter as Router } from "react-router-dom";
+import { HashRouter as Router } from "react-router-dom";
 
 function App() {
   return (

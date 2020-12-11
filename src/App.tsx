b9ccdd886a8f2879cf3cd55693be827b4c0efa99
Sarah <pappas.sarah@gmail.com>
2020-12-10 22:21:38 -0600

    Dynamically calculate vh for css. Add media query to Instructions.css.

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,11 @@ import { Main } from 'pages/Main/Main';
 import { HashRouter as Router } from "react-router-dom";
 
 function App() {
+  // For responsive CSS layout
+  // First we get the viewport height and we multiple it by 1% to get a value for a vh unit
+  const vh = window.innerHeight * 0.01;
+  // Then we set the value in the --vh custom property to the root of the document
+  document.documentElement.style.setProperty('--vh', `${vh}px`);
   return (
     <>
       <Router>
